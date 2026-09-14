0f1d932a1c58bf8998f048ee5e4d95b530e2755c
notoriY2 <notori.y2@gmail.com>
2026-09-14 21:43:24 +0200

    updated the mobile floor height

--- src/components/ProductFloor.tsx
+++ src/components/ProductFloor.tsx
@@ -431,7 +431,7 @@ const ProductFloor: React.FC<ProductFloorProps> = ({
 
   const mobileHeightVh = Math.max(
   55,
-  Math.max(0, ...floorEligibleProducts.map(p => parseVh(p.mobilePosition?.top ?? p.position.top))) + 16
+  Math.max(0, ...floorEligibleProducts.map(p => parseVh(p.mobilePosition?.top ?? p.position.top))) + 20
 );
 
   /* =========================================================
@@ -730,7 +730,7 @@ const ProductFloor: React.FC<ProductFloorProps> = ({
   className={`pt-0 relative ${
     viewMode ===
     'floor'
-      ? 'pb-16 sm:pb-48'
+      ? 'pb-20 sm:pb-48'
       : 'pb-12 sm:pb-16'
   }`}
 >
